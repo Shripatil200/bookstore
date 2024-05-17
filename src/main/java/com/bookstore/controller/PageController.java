f4f6a797e6f05bd9f63291ef2d6e3add1e169e67
Shripatil200 <patilshriram200@gmail.com>
2024-05-17 13:18:17 +0530

    created resposive navbar , sidebar, and content page

---
+++ src/main/java/com/bookstore/controller/PageController.java
@@ -0,0 +1,14 @@
+package com.bookstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping("/home")
+	public String homePage() {
+		return "home";
+	}
+
+}
