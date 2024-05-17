8ebbf9666ffb9da5c756eda781076dceff129691
Shripatil200 <patilshriram200@gmail.com>
2024-05-17 16:05:02 +0530

    working on responsivness

--- src/main/java/com/bookstore/controller/PageController.java
+++ src/main/java/com/bookstore/controller/PageController.java
@@ -3,6 +3,8 @@ package com.bookstore.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+
 @Controller
 public class PageController {
 	
@@ -11,4 +13,14 @@ public class PageController {
 		return "home";
 	}
 
+	@RequestMapping("/about")
+	public String aboutPage() {
+	    return "about";
+	}
+
+	@RequestMapping("/services")
+	public String servicesPage() {
+	    return "services";
+	}
+	
 }
